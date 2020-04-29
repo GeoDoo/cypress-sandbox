acfbc8fb6deab5f5faf4b6f012a12ce5c425736b
George Karametas <georgios.karametas@viooh.com>
2020-04-29 20:54:46 +0100

    Update UI and test login

--- cypress/integration/login.test.js
+++ cypress/integration/login.test.js
@@ -1,5 +1,7 @@
 describe("Login", () => {
   it("I should be able to login to the application", () => {
-    cy.login(Cypress.env("email"), Cypress.env("password"));
+    cy.fixture("user.json").then(({ email }) => {
+      cy.login(email, Cypress.env("password"));
+    });
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,8 @@
 Cypress.Commands.add("login", (email, password) => {
   cy.log(`Logging user with ${email}`);
+
+  cy.visit("/");
+
+  cy.get("#email").type(email);
+  cy.get("#password").type(password);
 });
