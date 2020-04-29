49fd412941b671af072e956e5671f2b299793323
George Karametas <georgios.karametas@viooh.com>
2020-04-29 20:37:02 +0100

    Add server

--- cypress/integration/login.test.js
+++ cypress/integration/login.test.js
@@ -1,5 +1,5 @@
 describe("Login", () => {
   it("I should be able to login to the application", () => {
-    cy.login();
+    cy.login(Cypress.env("email"), Cypress.env("password"));
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,3 @@
 Cypress.Commands.add("login", (email, password) => {
-  
+  cy.log(`Logging user with ${email}`);
 });
